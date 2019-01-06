60aff5f2c89f70695447cd0b8fb9ee7e53b74c93
Patrick Hacour <patrick.hacour@scarlet.be>
2019-01-06 21:34:00 +0100

    changed met foefke

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            tot ziens.
+            Nu met een foefke
           </p>
           <a
             className="App-link"
